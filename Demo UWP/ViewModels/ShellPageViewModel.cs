b042d1fba2d1c2048c174574ada539306dcf1160
Erik Weiss <eweiss@nerdery.com>
2016-08-03 10:17:29 -0500

    Set the homepage as the default selected page on startup

--- Demo UWP/ViewModels/ShellPageViewModel.cs
+++ Demo UWP/ViewModels/ShellPageViewModel.cs
@@ -62,26 +62,12 @@ namespace Demo_UWP.ViewModels
                 _container.UnregisterHandler(typeof(INavigationService), null);
 
             _navigationService = _container.RegisterNavigationService(frame);
+            SelectedMainNavigationItem = MainNavigationItems.First();
         }
 
-        /* 
-         * @erik - you had this strongly-typed as a ListBoxItem
-         * Caliburn was trying to pass in the associated ViewModel, not the UI item.
-         * 
-         * You can manipulate what item is passed into this method by setting different things 
-         * in the Message.Attach declaration in the XAML
-         * 
-         * This page will enumerate the options for you and is an all-around handy guide to Caliburn.Micro:
-         * http://caliburnmicro.codeplex.com/wikipage?title=Cheat%20Sheet&referringTitle=Documentation
-         * 
-         * As a troubleshooting tip in the future, when you get a null in one of these types of method calls again,
-         * try changing the parameter type to "object" and then inspect it at runtime.  A null generally
-         * means a bad cast was attempted.
-         * 
-        */
-        public void MenuItemSelected(ShellPageNavigationItemViewModel item)
+        public void MenuItemSelected()
         {
-            switch (item.Tag.ToString())
+            switch (SelectedMainNavigationItem.Tag)
             {
                 case "Home":
                     _navigationService.NavigateToViewModel<HomePageViewModel>();
